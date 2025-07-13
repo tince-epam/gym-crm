fc29e966e77aba7e7dd3bf74e250d5318ee82770
tutkuince <tutku.ince@outlook.com>
2025-07-13 19:59:57 +0300

    feat: add GymCrmApplication as Spring Boot application entry point

---
+++ src/main/java/com/epam/gymcrm/GymCrmApplication.java
@@ -0,0 +1,12 @@
+package com.epam.gymcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmApplication.class, args);
+    }
+}
