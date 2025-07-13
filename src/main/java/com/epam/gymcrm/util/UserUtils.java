3fdad7063802ccb564153ac7ae57767a92d5b922
tutkuince <tutku.ince@outlook.com>
2025-07-13 15:58:11 +0300

    feat: add UserUtils for username and password generation

--- src/main/java/com/epam/gymcrm/util/UserUtils.java
+++ src/main/java/com/epam/gymcrm/util/UserUtils.java
@@ -1,2 +1,30 @@
-package com.epam.gymcrm.util;public class UserUtils {
+package com.epam.gymcrm.util;
+
+import java.security.SecureRandom;
+import java.util.List;
+
+public class UserUtils {
+
+    private static final int PASSWORD_CHAR_LENGTH = 10;
+
+    public static String generateUniqueUsername(String firstName, String lastName, List<String> existingUsername) {
+        String baseUsername = firstName + "." + lastName;
+        String username = baseUsername;
+        int counter = 1;
+        while (existingUsername.contains(username)) {
+            username = baseUsername + counter;
+            counter++;
+        }
+        return username;
+    }
+
+    public static String generateRandomPassword() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(PASSWORD_CHAR_LENGTH);
+        for (int i = 0; i < PASSWORD_CHAR_LENGTH; i++) {
+            int ascii = 33 + random.nextInt(94);
+            sb.append((char) ascii);
+        }
+        return sb.toString();
+    }
 }
