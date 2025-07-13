ecb4181f3840b9ca2bcf846bda196515a08b5873
tutkuince <tutku.ince@outlook.com>
2025-07-13 16:18:34 +0300

    feat: implement GlobalExceptionHandler using @ControllerAdvice

--- src/main/java/com/epam/gymcrm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/epam/gymcrm/exception/GlobalExceptionHandler.java
@@ -1,2 +1,34 @@
-package com.epam.gymcrm.exception;public class GlobalExceptionHandler {
+package com.epam.gymcrm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TraineeNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTraineeNotFound(TraineeNotFoundException ex) {
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
